583dd283917aa6615234a7fa139e77f85665da4a
StarDragon <>
2026-05-23 16:58:41 -0400

    isolate js function scope

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+(function () {
 const BUILT_IN_DICE = ["1d4", "1d6", "1d8", "1d10", "1d12", "1d20", "1d100"];
 
 function escapeHtml(str) {
@@ -454,3 +455,4 @@ Hooks.once("ready", () => {
 });
 
 if (typeof module !== "undefined") module.exports = {};
+})();
